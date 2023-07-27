9148bcea2d7f4d0b61f0be41f05367130f955f2a
7-Stack <ashleyculture@yahoo.com>
2023-07-27 09:37:55 +0100

    Middlewares

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ connectDb();
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
+const basePath = "/api/v1"
 
 app.use(express.json());
-app.use("api/contacts", require("./routes/intro.routes"));
+app.use(`${basePath}/contacts`, require("./routes/intro.routes"));
+app.use(`${basePath}/users`, require("./routes/userRoutes"));
+
+app.get(`${basePath}/docs`, (req, res) => {
+
+    res.redirect("http://localhost:5002/api/users");
+});
+
 app.use(errorHandler);
 
 module.exports = app;
