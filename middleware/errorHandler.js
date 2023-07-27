fc332d9ee69d293cdfad0c44d3717744b062a623
7-Stack <ashleyculture@yahoo.com>
2023-07-27 09:28:34 +0100

    Token Logic

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,34 +1,35 @@
 const { constants } = require("../constants")
 const errorHandler = (err, req, res, next) => {
  const statusCode = res.statusCode ? res.statusCode : 500;
+ console.error(err)
  switch (statusCode) {
     case constants.VALIDATION_ERROR:
-     res.json({
+     return res.json({
        title: "Validation Failed",
        message: err.message,
        stackTrace: err.stack,
      });
      break;
     case constants.NOT_FOUND:
-     res.json({
+     return res.json({
        title: "Not Found",
        message: err.message,
        stackTrace: err.stack,
     });
     case constants.UNAUTHORIZED:
-     res.json({
+     return res.json({
        title: "Unauthorized",
        message: err.message,
        stackTrace: err.stack,
     });
     case constants.FORBIDDEN:
-     res.json({
+     return res.json({
        title: "Forbidden",
        message: err.message,
        stackTrace: err.stack,
     });
     case constants.SERVER_ERROR:
-     res.json({
+     return res.json({
        title: "Server Error",
        message: err.message,
        stackTrace: err.stack,
---
+++ middleware/validateTokenHandler.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const validateToken = async(req, res, next) => {
+    let token;
+    let authHeader = req.headers.authorization || req.headers.authorization;
+    if (authHeader &&  authHeader.startsWith("Bearer")) {
+        token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401);
+                throw new Error("User is not authorized");
+            }
+            req.user = decoded.user;
+        });
+    }
+}
+
+module.exports = validateToken;
